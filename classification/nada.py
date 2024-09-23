31363300a25bc5e0aa86478b753bca8ba2d94e38
hitoruna <hitorunajp@gmail.com>
2024-09-23 20:00:19 +0900

    getting the dataset and saving it

--- classification/nada.py
+++ classification/nada.py
@@ -1,6 +1,21 @@
 from classification import __version__ as version
 from config.core import get_config
 
-print(f"Version from nada {version}")
+from processing.data_manager import load_dataset
 
-print(f"Config from nada {get_config()}")
+# print(f"Version from nada {version}")
+# print(f"Config from nada {get_config()}")
+
+# config = get_config()
+
+def run(reread:bool)-> None:
+    data = load_dataset(reread)
+    if data is None:
+        print(f"No data loaded")
+        return
+    
+    print(data.head())
+
+
+if __name__ == "__main__":
+    run(True) #Reread value means to reread the source
---
+++ classification/processing/data_manager.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from classification.config.core import get_config,DATASET_DIR
+
+# def load_dataset(source):
+#     df = pd.read_csv(source)
+#     # replace interrogation marks by NaN values
+#     data = data.replace('?', np.nan)
+#     # Should we put more pre-processing here?
+
+config = get_config()
+
+def load_dataset(reread:bool):
+    saved_file = config.app_config.saved_data
+    saved_path = Path(f"{DATASET_DIR}/{saved_file}")
+    print(f"Saved data in {saved_path}")
+    source = config.app_config.data_source
+
+    if reread:
+        data = pd.read_csv(source)
+        # replace interrogation marks by NaN values
+        data = data.replace('?', np.nan)
+        # Other preprocessing here
+
+        # Then save the file
+        data.to_csv(saved_path, index=False)
+        print(f"Preprocessed data saved to {saved_path}")
+    else:
+        if saved_path.exists():
+            data = pd.read_csv(saved_path)
+        else:
+            print(f"{saved_path} does not exist.")
+            return None
+
+    return data
+
